2e8a60324435627ea57b96dbccd5a5f1fb19fa71
Venkatesh Katabathula <k.leelavenkatesh@gmail.com>
2017-10-04 19:06:25 +0530

    Now router works as expected

---
+++ taxi-server/controllers/main-route.js
@@ -0,0 +1,12 @@
+const express = require('express'),
+    router = express.Router();
+const app = express();
+
+//URL's without tokens
+router.get('/', function (req, res, next) {
+    res.send('indexExpress');
+    res.end();
+});
+
+router.use('/', require('./login-routes'));
+module.exports = router;
--- taxi-server/server.js
+++ taxi-server/server.js
@@ -3,20 +3,28 @@ const cors = require('cors'),
     express = require('express'),
     bodyParser = require('body-parser'),
     cookieParser = require('cookie-parser'),
-    mainRoute = require('./controllers/mainroute'),
+    mainRoute = require('./controllers/main-route'),
     config = require('./helpers/config');
 
 const router = express.Router();
 
 const app = express();
-
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/api', mainRoute);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
 http.createServer(app).listen(config.port, function (err) {
     console.log('server is listening on ',config.hostname,':',config.port);
 });
+
+module.exports = app;
